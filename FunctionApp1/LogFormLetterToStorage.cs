31ad7f77a73eb20ca038324c4e235b869a9b6429
Ben Hardesty <benjamint.hardesty@gmail.com>
2020-02-24 17:12:25 -0800

    LogFormLetterToStorage class completed

--- FunctionApp1/LogFormLetterToStorage.cs
+++ FunctionApp1/LogFormLetterToStorage.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading.Tasks;
 using FunctionApp1.Messages;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
 
@@ -16,9 +15,20 @@ namespace FunctionApp1
             ILogger log)
         {
 
-            //TODO map FormLetter message to LetterEntity type and save to table storage
-            
-            await letterTableCollector.AddAsync(new LetterEntity { });
+            Random rand = new Random();
+            LetterEntity letterEntity = new LetterEntity
+            {
+                Heading = myQueueItem.Heading,
+                Likelihood = myQueueItem.Likelihood,
+                ExpectedDate = myQueueItem.ExpectedDate,
+                RequestedDate = myQueueItem.RequestedDate,
+                Body = myQueueItem.Body,
+                PartitionKey = myQueueItem.Heading.ToLower(),
+                RowKey = ((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds).ToString() +
+                         rand.Next(10000, 99000).ToString() // Unix timestamp + random 5 digit integer
+            };
+
+            await letterTableCollector.AddAsync(letterEntity);
 
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
 
